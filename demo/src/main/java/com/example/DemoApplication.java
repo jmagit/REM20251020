7b94255ef9620b30413e95cd72addcdf6e203292
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-21 12:50:14 +0200

    Ejemplos de AOP

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.event.EventListener;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
@@ -26,6 +27,7 @@ import com.example.ioc.contratos.ServicioCadenas;
 import com.example.ioc.notificaciones.ConstructorConValores;
 import com.example.ioc.notificaciones.Sender;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
